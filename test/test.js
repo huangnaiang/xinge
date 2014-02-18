4fec244529da6bb57c939f45b9aeb0e72da51208
马路天使 <huangnaiang@gmail.com>
2014-02-18 18:18:44 +0800

    修复bug

--- test/test.js
+++ test/test.js
@@ -1,3 +1,14 @@
+
+// var a = {
+// 	name : 'av',
+// 	url: 'http://elseif.me'
+// };
+// a = JSON.stringify(a);
+// //a = JSON.stringify(a);
+// console.log(a);
+// return;
+
+
 var Xinge = require('../lib/Xinge');
 var XingeApp = new Xinge.XingeApp('2100001932', 'ebaf10ce868fec205efc0706e8bc5312');
 
@@ -6,33 +17,33 @@ var action = new Xinge.ClickAction(Xinge.ACTION_TYPE_BROWSER, null, 'http://xg.q
 
 var message = new Xinge.Message();
 message.type = Xinge.MESSAGE_TYPE_NOTIFICATION;
-message.title = '1';
+message.title = '你';
 message.content = '2';
-message.sendTime = 1387446540;
 message.style = style;
 message.action = action;
 
-XingeApp.pushToSingleDevice('c682191aa2aedbc44fbabe5dff99eb037c696338', message, 400, function(err, result){
+//推送消息给指定设备
+XingeApp.pushToSingleDevice('c2824dae0c9e8ed34f9aa77581772bbb638ed325', message, 400, function(err, result){
  	console.log(result);
 });
  
 
-XingeApp.pushToAllDevices(Xinge.DEVICE_TYPE_ALL, message, 400, function(err, result){
-	console.log(result);
-});
+// XingeApp.pushToAllDevices(Xinge.DEVICE_TYPE_ALL, message, 400, function(err, result){
+// 	console.log(result);
+// });
 
-XingeApp.queryPushStatus([516, 517, 518], 400, function(err, result){
-	console.log(result);
-});
+// XingeApp.queryPushStatus([516, 517, 518], 400, function(err, result){
+// 	console.log(result);
+// });
 
-XingeApp.queryDeviceCount(400, function(err, result){
-	console.log(result);
-});
+// XingeApp.queryDeviceCount(400, function(err, result){
+// 	console.log(result);
+// });
 
-XingeApp.queryAppTags(0, 100, 400, function(err, result){
-	console.log(result);
-});
+// XingeApp.queryAppTags(0, 100, 400, function(err, result){
+// 	console.log(result);
+// });
 
-XingeApp.cancelTimingTask('1', 400, function(err, result){
-	console.log(result);
-});
+// XingeApp.cancelTimingTask('1', 400, function(err, result){
+// 	console.log(result);
+// });
