2f94f096707d70356b47463017cc7ccc6de94e94
马路天使 <huangnaiang@gmail.com>
2014-03-20 11:44:23 +0800

    支持多包名、ios推送

--- test/test.js
+++ test/test.js
@@ -1,54 +1,68 @@
 var Xinge = require('../lib/Xinge');
-var XingeApp = new Xinge.XingeApp(2100000000, 'eb32f10ce868fec205efccd46e8bc53ds');
 
-var style = new Xinge.Style(0,1,1,1);
+var accessId  = 2200002957;
+var secretKey = 'b406a22207c15f1278939d4e58e9dffe';
+var XingeApp = new Xinge.XingeApp(accessId, secretKey);
+
+//Android message start.
+var style = new Xinge.Style(0, 1, 1, 1, 0);
 var action = new Xinge.ClickAction(Xinge.ACTION_TYPE_BROWSER, null, 'http://xg.qq.com', 1);
-
-var message = new Xinge.Message();
-message.type = Xinge.MESSAGE_TYPE_NOTIFICATION;
-message.title = '你好';
-message.content = '这是一条来自Node API的测试消息';
-message.style = style;
-message.action = action;
-message.sendTime = Date.parse('2014-02-19 15:33:30') / 1000;
-message.expireTime = 86400;
-message.acceptTime.push(new Xinge.TimeInterval(7, 0, 8, 0));
-message.acceptTime.push(new Xinge.TimeInterval(13, 0, 23, 59));
-message.customContent = {
-	'key': 'value',
-	'name': 'huangnaiang',
-	'age': '24'
+var androidMessage = new Xinge.AndroidMessage();
+androidMessage.type = Xinge.MESSAGE_TYPE_NOTIFICATION;
+androidMessage.title = '1';
+androidMessage.content = '2';
+androidMessage.style = style;
+androidMessage.action = action;
+androidMessage.sendTime = Date.parse('2014-02-19 15:33:30') / 1000;
+androidMessage.expireTime = 86400;
+androidMessage.acceptTime.push(new Xinge.TimeInterval(0, 0, 23, 59));
+androidMessage.customContent = {
+	'name': 'huangnaiang'
 };
+androidMessage.multiPkg = 0;
+//And message end.
+
+//IOS message start.
+var iOSMessage = new Xinge.IOSMessage();
+iOSMessage.alert = 'av';
+iOSMessage.badge = 22;
+iOSMessage.sound = 'df';
+iOSMessage.acceptTime.push(new Xinge.TimeInterval(0, 0, 23, 0));
+iOSMessage.customContent = {
+    key1: 'value1',
+    key2: 'value2'
+};
+//IOS message end.
 
 //推送消息给指定设备
-XingeApp.pushToSingleDevice('token', message, function(err, result){
- 	console.log(result);
-});
+XingeApp.pushToSingleDevice('e3ab84d2d0673c8776bb831a093fe61bea2180639993fa734bb5f0a686188eb5', iOSMessage, function(err, result){
+	console.log(result);
+}, exports.IOS_ENV_DEV);
 
 //推送消息给指定账户或别名
-XingeApp.pushToSingleAccount(Xinge.DEVICE_TYPE_ALL, 'account', message, function(err, result){
-	console.log(result);
-});
- 
-//推送消息给所有设备
-XingeApp.pushToAllDevices(Xinge.DEVICE_TYPE_ANDROID, message, function(err, result){
+XingeApp.pushToSingleAccount(Xinge.DEVICE_TYPE_ALL, 'account', androidMessage, function(err, result){
 	console.log(result);
 });
 
+//推送消息给所有设备
+XingeApp.pushToAllDevices(Xinge.DEVICE_TYPE_ALL, iOSMessage, function(err, result){
+	console.log(result);
+}, exports.IOS_ENV_DEV);
+
 //推送消息给指定tag
-XingeApp.pushByTags(Xinge.DEVICE_TYPE_IOS, ['123', 'abc'], Xinge.TAG_OPERATION_OR, message, function(err, result){
+XingeApp.pushByTags(Xinge.DEVICE_TYPE_IOS, ['av'], Xinge.TAG_OPERATION_OR, androidMessage, function(err, result){
 	console.log(result);
 });
 
 //批量查询消息推送状态
-XingeApp.queryPushStatus(['703', 704, '702'], function(err, result){
+XingeApp.queryPushStatus(['2824'], function(err, result){
 	console.log(result);
 });
 
 //查询设备数量
 XingeApp.queryDeviceCount(function(err, result){
 	console.log(result);
-}); 
+});
 
 //查询tag
 XingeApp.queryTags(0, 100, function(err, result){
