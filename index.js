9e5a5633263d3e96579ad472ce4dd679945b6610
无星 <329106954@qq.com>
2020-01-15 17:25:27 +0800

    添加导出

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-module.exports = require('./lib/Xinge');
+exports.XingePush = require('./lib/xinge_push')
+exports.XingeTag = require('./lib/xinge_tag')
+exports.XingeAccount = require('./lib/xinge_account')
+exports.XingeUntils = require('./lib/untils/xinge_untils')
+exports.XingeAndroidAction = require('./lib/message/xinge_android_action')
+exports.XingeAndroidMessage = require('./lib/message/xinge_android_message')
+exports.XingeAndroidStyle = require('./lib/message/xinge_android_style')
+exports.TimeIntervalV3 = require('./lib/message/xinge_android_timeinterval')
+exports.XingeIOSMessage = require('./lib/message/xinge_ios_message')
+const {
+  XingeUrls,
+  XingeAudienceType,
+  XingePlatform,
+  XingeMessageType,
+  XingeEnv,
+  XingeActionType,
+  XingeTagOperation,
+  XingeTagOperatorType,
+  XingeAccountOperatorType,
+  XingeOpType,
+  XingeAccountType
+} = require('./lib/const/xinge_const')
+exports.XingeUrls = XingeUrls
+exports.XingeAudienceType = XingeAudienceType
+exports.XingePlatform = XingePlatform
+exports.XingeMessageType = XingeMessageType
+exports.XingeEnv = XingeEnv
+exports.XingeActionType = XingeActionType
+exports.XingeTagOperation = XingeTagOperation
+
+exports.XingeTagOperatorType = XingeTagOperatorType
+
+exports.XingeAccountOperatorType = XingeAccountOperatorType
+exports.XingeOpType = XingeOpType
+exports.XingeAccountType = XingeAccountType
